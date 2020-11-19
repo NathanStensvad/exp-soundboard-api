03fcf8e6014af973b6496c6db1589ff23a1f5319
Nathan Stensvad <nstensvad@hotmail.com>
2020-11-18 18:34:07 -0800

    Changes for client interaction

--- src/app.js
+++ src/app.js
@@ -22,8 +22,14 @@ app.use(
     })
 );
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
 })
 
 app.use(soundboardsRouter)
