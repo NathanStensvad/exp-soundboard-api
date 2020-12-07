eea82a8c4658066fb176ab78877010e90d4046e5
Nathan Stensvad <nstensvad@hotmail.com>
2020-12-07 11:36:50 -0800

    updated database name

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dumy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://nathan@localhost/exp-soundboard',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://nathan@localhost/exp-soundboard',
 }
